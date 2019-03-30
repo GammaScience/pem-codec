a2061251d1e4b3da16e7067d45cfc13a978b8de8
Roger Gammans <rgammans@gammascience.co.uk>
2019-03-30 16:17:32 +0000

    Fix so compilation and lint errors.
    Also move self test call into spec file.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 
 
-interface PEM_header {
+export interface PEM_header {
     name : string ;
     value: string;
 }
-interface PEM_message {
+export interface PEM_message {
     type: string;
     data:   Uint8Array;
     headers:    Array<PEM_header> ;
@@ -12,17 +12,17 @@ interface PEM_message {
 }
 class PEMh {
     name: string;
-    value: string;  # this is often a comma separated list
+    value: string;  // this is often a comma separated list
     toString() {
-        return this.name + ': ' + this.value; # FIXME - wrap name/value to N chars wide; breaking on commas
+        return this.name + ': ' + this.value; // FIXME - wrap name/value to N chars wide; breaking on commas
     }
 }
 
 class PEM {
     type: string;
     data:   Uint8Array;
-    headers:    Array<PEM_header> =new Array(); # may be empty
-    pre_headers:    Array<PEM_header> =new Array(); # may be empty
+    headers:    Array<PEM_header> =new Array(); // may be empty
+    pre_headers:    Array<PEM_header> =new Array(); // may be empty
 }
 
 
@@ -43,13 +43,13 @@ var header_regexp = new RegExp(HDR,'gm');
 var main_regexp = new RegExp( MAIN , 'g');
 
 
-function decode(msg: string) {
+export function decode(msg: string) : PEM_message {
     let decoded_msg: PEM_message = new PEM();
     var vals;
     var parts;
-    if ((vals = r0.exec(msg)) != null){
+    if ((vals = main_regexp.exec(msg)) != null){
         decoded_msg.type = vals[1];
-        while ((parts = r1.exec(vals[2])) != null){
+        while ((parts = header_regexp.exec(vals[2])) != null){
             var hdr: PEM_header = new PEMh();
             hdr.name = parts[1];
             hdr.value= [parts[2],parts[3]].join('') ;
@@ -66,7 +66,7 @@ function decode(msg: string) {
     return decoded_msg;
 }
 
-function encode(msg: PEM_message){
+export function encode(msg: PEM_message) : string {
     let encoded_msg: string;
     var pre: string[] = [];
     var line;
@@ -78,131 +78,9 @@ function encode(msg: PEM_message){
         hdr.push((msg.headers[line]).toString());  // FIXME wrap line.value
     }
     
-    var data:string = msg.data.toString();  # FIXME convert from int array to str representation
+    var data:string = msg.data.toString();  // FIXME convert from int array to str representation
 
     encoded_msg = [pre, DASHES + OPEN + ' ' + msg.type + DASHES, hdr, data, DASHES + CLOSE + ' ' + msg.type + DASHES].join('\n');
     
     return encoded_msg;
 }
-var mx:string = encode(decode(msg));    # round-robin the message
-console.log(mx);
-
-
-############use cases#######################
-#PEM cert with explanatory txt
-
-/*
-Subject: CN=Atlantis
-Issuer: CN=Atlantis
-Validity: from 7/9/2012 3:10:38 AM UTC to 7/9/2013 3:10:37 AM UTC
------BEGIN CERTIFICATE-----
-MIIBmTCCAUegAwIBAgIBKjAJBgUrDgMCHQUAMBMxETAPBgNVBAMTCEF0bGFudGlz
-MB4XDTEyMDcwOTAzMTAzOFoXDTEzMDcwOTAzMTAzN1owEzERMA8GA1UEAxMIQXRs
-YW50aXMwXDANBgkqhkiG9w0BAQEFAANLADBIAkEAu+BXo+miabDIHHx+yquqzqNh
-Ryn/XtkJIIHVcYtHvIX+S1x5ErgMoHehycpoxbErZmVR4GCq1S2diNmRFZCRtQID
-AQABo4GJMIGGMAwGA1UdEwEB/wQCMAAwIAYDVR0EAQH/BBYwFDAOMAwGCisGAQQB
-gjcCARUDAgeAMB0GA1UdJQQWMBQGCCsGAQUFBwMCBggrBgEFBQcDAzA1BgNVHQEE
-LjAsgBA0jOnSSuIHYmnVryHAdywMoRUwEzERMA8GA1UEAxMIQXRsYW50aXOCASow
-CQYFKw4DAh0FAANBAKi6HRBaNEL5R0n56nvfclQNaXiDT174uf+lojzA4lhVInc0
-ILwpnZ1izL4MlI9eCSHhVQBHEp2uQdXJB+d5Byg=
------END CERTIFICATE-----
-*/
-/*
------BEGIN PRIVACY-ENHANCED MESSAGE-----
-Proc-Type: 4,MIC-ONLY
-Content-Domain: RFC822
-Originator-Certificate:
- MIIBlTCCAScCAWUwDQYJKoZIhvcNAQECBQAwUTELMAkGA1UEBhMCVVMxIDAeBgNV
- BAoTF1JTQSBEYXRhIFNlY3VyaXR5LCBJbmMuMQ8wDQYDVQQLEwZCZXRhIDExDzAN
- BgNVBAsTBk5PVEFSWTAeFw05MTA5MDQxODM4MTdaFw05MzA5MDMxODM4MTZaMEUx
- CzAJBgNVBAYTAlVTMSAwHgYDVQQKExdSU0EgRGF0YSBTZWN1cml0eSwgSW5jLjEU
- MBIGA1UEAxMLVGVzdCBVc2VyIDEwWTAKBgRVCAEBAgICAANLADBIAkEAwHZHl7i+
- yJcqDtjJCowzTdBJrdAiLAnSC+CnnjOJELyuQiBgkGrgIh3j8/x0fM+YrsyF1u3F
- LZPVtzlndhYFJQIDAQABMA0GCSqGSIb3DQEBAgUAA1kACKr0PqphJYw1j+YPtcIq
- iWlFPuN5jJ79Khfg7ASFxskYkEMjRNZV/HZDZQEhtVaU7Jxfzs2wfX5byMp2X3U/
- 5XUXGx7qusDgHQGs7Jk9W8CW1fuSWUgN4w==
-Issuer-Certificate:
- MIIB3DCCAUgCAQowDQYJKoZIhvcNAQECBQAwTzELMAkGA1UEBhMCVVMxIDAeBgNV
- BAoTF1JTQSBEYXRhIFNlY3VyaXR5LCBJbmMuMQ8wDQYDVQQLEwZCZXRhIDExDTAL
- BgNVBAsTBFRMQ0EwHhcNOTEwOTAxMDgwMDAwWhcNOTIwOTAxMDc1OTU5WjBRMQsw
- CQYDVQQGEwJVUzEgMB4GA1UEChMXUlNBIERhdGEgU2VjdXJpdHksIEluYy4xDzAN
- BgNVBAsTBkJldGEgMTEPMA0GA1UECxMGTk9UQVJZMHAwCgYEVQgBAQICArwDYgAw
- XwJYCsnp6lQCxYykNlODwutF/jMJ3kL+3PjYyHOwk+/9rLg6X65B/LD4bJHtO5XW
- cqAz/7R7XhjYCm0PcqbdzoACZtIlETrKrcJiDYoP+DkZ8k1gCk7hQHpbIwIDAQAB
- MA0GCSqGSIb3DQEBAgUAA38AAICPv4f9Gx/tY4+p+4DB7MV+tKZnvBoy8zgoMGOx
- dD2jMZ/3HsyWKWgSF0eH/AJB3qr9zosG47pyMnTf3aSy2nBO7CMxpUWRBcXUpE+x
- EREZd9++32ofGBIXaialnOgVUn0OzSYgugiQ077nJLDUj0hQehCizEs5wUJ35a5h
-MIC-Info: RSA-MD5,RSA,
- jV2OfH+nnXHU8bnL8kPAad/mSQlTDZlbVuxvZAOVRZ5q5+Ejl5bQvqNeqOUNQjr6
- EtE7K2QDeVMCyXsdJlA8fA==
-
-LSBBIG1lc3NhZ2UgZm9yIHVzZSBpbiB0ZXN0aW5nLg0KLSBGb2xsb3dpbmcgaXMg
-YSBibGFuayBsaW5lOg0KDQpUaGlzIGlzIHRoZSBlbmQuDQo=
------END PRIVACY-ENHANCED MESSAGE-----
-*/
-
-
-#Encapsulated Message (Symmetric Case)
-/*
------BEGIN PRIVACY-ENHANCED MESSAGE-----
-Proc-Type: 4,ENCRYPTED
-Content-Domain: RFC822
-DEK-Info: DES-CBC,F8143EDE5960C597
-Originator-ID-Symmetric: linn@zendia.enet.dec.com,,
-Recipient-ID-Symmetric: linn@zendia.enet.dec.com,ptf-kmc,3
-Key-Info: DES-ECB,RSA-MD2,9FD3AAD2F2691B9A,
-          B70665BB9BF7CBCDA60195DB94F727D3
-Recipient-ID-Symmetric: pem-dev@tis.com,ptf-kmc,4
-Key-Info: DES-ECB,RSA-MD2,161A3F75DC82EF26,
-          E2EF532C65CBCFF79F83A2658132DB47
-
-LLrHB0eJzyhP+/fSStdW8okeEnv47jxe7SJ/iN72ohNcUk2jHEUSoH1nvNSIWL9M
-8tEjmF/zxB+bATMtPjCUWbz8Lr9wloXIkjHUlBLpvXR0UrUzYbkNpk0agV2IzUpk
-J6UiRRGcDSvzrsoK+oNvqu6z7Xs5Xfz5rDqUcMlK1Z6720dcBWGGsDLpTpSCnpot
-dXd/H5LMDWnonNvPCwQUHt==
------END PRIVACY-ENHANCED MESSAGE-----
-*/
-/*
------BEGIN PRIVACY-ENHANCED MESSAGE-----
-Proc-Type: 4,ENCRYPTED
-Content-Domain: RFC822
-DEK-Info: DES-CBC,BFF968AA74691AC1
-Originator-Certificate:
- MIIBlTCCAScCAWUwDQYJKoZIhvcNAQECBQAwUTELMAkGA1UEBhMCVVMxIDAeBgNV
- BAoTF1JTQSBEYXRhIFNlY3VyaXR5LCBJbmMuMQ8wDQYDVQQLEwZCZXRhIDExDzAN
- BgNVBAsTBk5PVEFSWTAeFw05MTA5MDQxODM4MTdaFw05MzA5MDMxODM4MTZaMEUx
- CzAJBgNVBAYTAlVTMSAwHgYDVQQKExdSU0EgRGF0YSBTZWN1cml0eSwgSW5jLjEU
- MBIGA1UEAxMLVGVzdCBVc2VyIDEwWTAKBgRVCAEBAgICAANLADBIAkEAwHZHl7i+
- yJcqDtjJCowzTdBJrdAiLAnSC+CnnjOJELyuQiBgkGrgIh3j8/x0fM+YrsyF1u3F
- LZPVtzlndhYFJQIDAQABMA0GCSqGSIb3DQEBAgUAA1kACKr0PqphJYw1j+YPtcIq
- iWlFPuN5jJ79Khfg7ASFxskYkEMjRNZV/HZDZQEhtVaU7Jxfzs2wfX5byMp2X3U/
- 5XUXGx7qusDgHQGs7Jk9W8CW1fuSWUgN4w==
-Key-Info: RSA,
- I3rRIGXUGWAF8js5wCzRTkdhO34PTHdRZY9Tuvm03M+NM7fx6qc5udixps2Lng0+
- wGrtiUm/ovtKdinz6ZQ/aQ==
-Issuer-Certificate:
- MIIB3DCCAUgCAQowDQYJKoZIhvcNAQECBQAwTzELMAkGA1UEBhMCVVMxIDAeBgNV
- BAoTF1JTQSBEYXRhIFNlY3VyaXR5LCBJbmMuMQ8wDQYDVQQLEwZCZXRhIDExDTAL
- BgNVBAsTBFRMQ0EwHhcNOTEwOTAxMDgwMDAwWhcNOTIwOTAxMDc1OTU5WjBRMQsw
- CQYDVQQGEwJVUzEgMB4GA1UEChMXUlNBIERhdGEgU2VjdXJpdHksIEluYy4xDzAN
- BgNVBAsTBkJldGEgMTEPMA0GA1UECxMGTk9UQVJZMHAwCgYEVQgBAQICArwDYgAw
- XwJYCsnp6lQCxYykNlODwutF/jMJ3kL+3PjYyHOwk+/9rLg6X65B/LD4bJHtO5XW
- cqAz/7R7XhjYCm0PcqbdzoACZtIlETrKrcJiDYoP+DkZ8k1gCk7hQHpbIwIDAQAB
- MA0GCSqGSIb3DQEBAgUAA38AAICPv4f9Gx/tY4+p+4DB7MV+tKZnvBoy8zgoMGOx
- dD2jMZ/3HsyWKWgSF0eH/AJB3qr9zosG47pyMnTf3aSy2nBO7CMxpUWRBcXUpE+x
- EREZd9++32ofGBIXaialnOgVUn0OzSYgugiQ077nJLDUj0hQehCizEs5wUJ35a5h
-MIC-Info: RSA-MD5,RSA,
- UdFJR8u/TIGhfH65ieewe2lOW4tooa3vZCvVNGBZirf/7nrgzWDABz8w9NsXSexv
- AjRFbHoNPzBuxwmOAFeA0HJszL4yBvhG
-Recipient-ID-Asymmetric:
- MFExCzAJBgNVBAYTAlVTMSAwHgYDVQQKExdSU0EgRGF0YSBTZWN1cml0eSwgSW5j
- LjEPMA0GA1UECxMGQmV0YSAxMQ8wDQYDVQQLEwZOT1RBUlk=,
- 66
-Key-Info: RSA,
- O6BS1ww9CTyHPtS3bMLD+L0hejdvX6Qv1HK2ds2sQPEaXhX8EhvVphHYTjwekdWv
- 7x0Z3Jx2vTAhOYHMcqqCjA==
-
-qeWlj/YJ2Uf5ng9yznPbtD0mYloSwIuV9FRYx+gzY+8iXd/NQrXHfi6/MhPfPF3d
-jIqCJAxvld2xgqQimUzoS1a4r7kQQ5c/Iua4LqKeq3ciFzEv/MbZhA==
------END PRIVACY-ENHANCED MESSAGE-----
-*/
